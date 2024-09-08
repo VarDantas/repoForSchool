a655d9970e55b4199a2d471f420e8012cbd091f2
Varvara Kudria <144835042+VarDantas@users.noreply.github.com>
2024-09-08 14:14:30 +0300

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,12 @@
+file = open("data.txt")
+k = 0
+for line in file:
+    flag = 1
+    arr = [int(x) for x in line.split(';')]
+    for i in range(7):
+        for j in range(i+1,7):
+            if (arr[i]==arr[j]):
+                flag = 0
+    if (flag == 1) and ((sum(arr)/7)%11==0):
+        k+=1
+print(k)
