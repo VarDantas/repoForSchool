af562cc956fa868b089083a6c54f7b617a2ec91f
Varvara Kudria <144835042+VarDantas@users.noreply.github.com>
2024-09-13 22:24:44 +0300

    num17636.py

--- дз 19.09/num17636.py
+++ дз 19.09/num17636.py
@@ -2,10 +2,10 @@ f = open("17_17636.txt")
 arr = [int(x) for x in f]
 maxSum = -300001
 count = 0
-arrMax3 = [x for x in arr if ((x%10==3)and(x//1000==0))]
+arrMax3 = [x for x in arr if ((abs(x)%10==3)and(len(str(abs(x))))==3)]
 max3 = max(arrMax3)
 for i in range(1, len(arr)-1):
-    arr3 = [x for x in [arr[i-1], arr[i], arr[i+1]] if ((x%10==3)and(x//1000==0))]
+    arr3 = [x for x in [arr[i-1], arr[i], arr[i+1]] if ((abs(x)%10==3)and(len(str(abs(x))))==3)]
     if (len(arr3) > 0) and (arr[i-1]+arr[i]+arr[i+1] < max3):
         count += 1
         maxSum = max(maxSum, arr[i]+arr[i-1]+arr[i+1])
