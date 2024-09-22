1391fc07712fa3c39e9d1a0c15cb4c605f570d6d
Varvara Kudria <144835042+VarDantas@users.noreply.github.com>
2024-09-22 20:17:12 +0300

    num11481.py

--- дз 26.09/num11481.py
+++ дз 26.09/num11481.py
@@ -5,7 +5,7 @@ arr = [ int(x) for x in f]
 arr8 = [ x for x in arr if str(x)[0] == '8']
 max8 = max(arr8)
 for i in range(1, len(arr)-1):
-    arr6 = [x for x in [arr[i], arr[i-1], arr[i+1]] if str(x)[0] == '6']
+    arr6 = [x for x in [arr[i], arr[i-1], arr[i+1]] if str(abs(x))[0] == '6']
     if (len(arr6) <= 1) and (arr[i] + arr[i-1] + arr[i+1] >= max8):
         count += 1
         minSum = min(minSum, arr[i] + arr[i-1] + arr[i+1])
