cba92ba055d7b24f2c2936e6be54667ccd563f70
Varvara Kudria <144835042+VarDantas@users.noreply.github.com>
2024-11-23 11:19:20 +0300

    Add files via upload

---
+++ дз введение в it 28.11.24/6.1.py
@@ -0,0 +1,11 @@
+def f(s,c,n):
+    if s+c >= 65 or n > 2:
+        return n == 2
+    if n % 2 == 0:
+        return f(s+1,c,n+1) or f(s,c+1,n+1) or f(s*3,c,n+1) or f(s,c*3,n+1)
+    return f(s+1,c,n+1) or f(s,c+1,n+1) or f(s*3,c,n+1) or f(s,c*3,n+1)
+
+for s in range(2, 58):
+    if f(s,6,0):
+        print(s)
+    
---
+++ дз введение в it 28.11.24/6.2.py
@@ -0,0 +1,11 @@
+def f(s,c,n):
+    if s+c >= 65 or n > 3:
+        return n == 3
+    if n % 2 == 1:
+        return f(s+1,c,n+1) and f(s,c+1,n+1) and f(s*3,c,n+1) and f(s,c*3,n+1)
+    return f(s+1,c,n+1) or f(s,c+1,n+1) or f(s*3,c,n+1) or f(s,c*3,n+1)
+
+for s in range(2, 58):
+    if f(s,6,0):
+        print(s)
+    
